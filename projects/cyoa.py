900df99680272ee0abff9aa266eb307a4efedea1
Aldair <asanch@ad.unc.edu>
2021-02-27 18:42:18 -0500

    progress

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -1,11 +1,27 @@
 """My hangman game"""
 
+def greet() -> None:
+    """Greeting to the player and introduction."""
+    print("Hello and welcome to my Hangman game!")
+    print("In this game you will be trying to guess the word one letter at a time")
+    player: str = str(input("Name? "))
+    print(f"{player} please choose which option you would like")
+    print("1: Easy, 2: Hard, Or 3: Main menu")
+
+
+def experience(choice: int) -> str: 
+    if choice == 1:
+        return "Great lets get started"
+    else:
+        return "Maybe next time"
+
+
+
 def main() -> None: 
-    """Start of my program."""
-    print("Welcome to my hangman game")
+    print(greet())
+    option: int = int(input("Option? "))
+    print(experience(option))
 
 
-
-
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
