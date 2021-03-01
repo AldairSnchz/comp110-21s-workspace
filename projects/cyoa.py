432f2e14a406fc2b1c10e806a4f7293eabae69d8
Aldair <asanch@ad.unc.edu>
2021-02-28 21:11:02 -0500

    finished

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -1,7 +1,13 @@
-"""My Guessing game"""
+"""My Guessing game!"""
 
 
-def greet() -> str:                            #1 Procedure nameed greet
+from random import randint
+
+
+__author__ = "730447704"
+
+
+def greet() -> str:                            # 1 Procedure nameed greet
     """Greeting to the player and introduction."""
     return f"Hello and welcome to my guessing game! {intr}, {other}, {the_three}"
 
@@ -10,13 +16,13 @@ intr: str = "In this game you will be trying to guess the letter i'm thinking of
 other: str = "please choose which option you would like"
 the_three: str = "1: Main game, 2: Bonus game, or 3: Main menu"
 mad_emoji: str = "\U0001F643"
-my_emoji: str = "\U0001F605"                #7 emoji global variables
+my_emoji: str = "\U0001F605"                # 7 emoji global variables
 end_emoji: str = "\U0001F44D"
-player: str = str(input("Name? "))          #Player global variable
+player: str = str(input("Name? "))          # Player global variable
 op_one: str = "You will now try to guess what letter im thinking of in the fewest attempts"
 
 
-def option_one() -> str:              #4 procedure call
+def option_one() -> str:              # 4 procedure call
     """This is where the game will take place."""
     print(f"Great {player} this is the main game! {op_one}")
     points: int = 0
@@ -38,26 +44,23 @@ def extra_option() -> str:
         return "Okay that's fine, have a good day!"
 
 
-def option_two(a: int) -> int:
-    """This is the bonus round"""
+def option_two(a: int) -> int:                  # 5 function with int parameter and return int
+    """This is the bonus round."""
     points: int = int(input(f"{player} how many tries did the first round take? "))
     if a < 10:
         return points - 1
     else:
         return points + 1
-    
-    
-from random import randint
 
 
 def option_three() -> str:
     """Prints adventure points and says goodbye."""
-    points: int = 0
+    points: int = 0                 # 2 points in main
     x: int = 1
     y: int = 3
-    leave: int = randint(x, y)
+    leave: int = randint(x, y)              # 8 random
     if leave == 1:
-        return f"You took {points} guesses so you must be good! Come again soon!"
+        return f"You took {points} guesses so you must be good! Come again soon!"       # 6 f string
     else:
         if leave == 2:
             return f"{points} guesses, you should have tried my game {mad_emoji}"
@@ -65,12 +68,12 @@ def option_three() -> str:
             return f"You suck, you took {points} guesses and didn't even try {mad_emoji}"
 
 
-def main() -> None:                         #0 Main function that returns none
+def main() -> None:                         # 0 Main function that returns none
     """Where everything is going down."""
     print(greet())
     option: int = int(input("Option? "))
     points: int = 0
-    if option == 1:                         #3 presenting three options and taking you to them
+    if option == 1:                         # 3 presenting three options and taking you to them
         print(option_one())
         print(extra_option())
     else:
@@ -79,7 +82,6 @@ def main() -> None:                         #0 Main function that returns none
             print(f"Your final score is -1 because you didn't even try {mad_emoji}")
         else:
             print(option_three())
-
 
 
 if __name__ == "__main__":      
