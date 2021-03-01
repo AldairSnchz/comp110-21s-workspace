4ca047e49b72834912b33ae12fcd8b461cbc474a
Aldair <asanch@ad.unc.edu>
2021-02-28 23:33:27 -0500

    now finished

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -7,24 +7,43 @@ from random import randint
 __author__ = "730447704"
 
 
-def greet() -> str:                            # 1 Procedure nameed greet
-    """Greeting to the player and introduction."""
-    return f"Hello and welcome to my guessing game! {intr}, {other}, {the_three}"
-
-
 intr: str = "In this game you will be trying to guess the letter i'm thinking of"
 other: str = "please choose which option you would like"
 the_three: str = "1: Main game, 2: Bonus game, or 3: Main menu"
 mad_emoji: str = "\U0001F643"
 my_emoji: str = "\U0001F605"                # 7 emoji global variables
 end_emoji: str = "\U0001F44D"
-player: str = str(input("Name? "))          # Player global variable
+player: str                                 # Player global variable
 op_one: str = "You will now try to guess what letter im thinking of in the fewest attempts"
+points: int
+
+
+def main() -> None:                         # 0 Main function that returns none
+    """Where everything is going down."""
+    print(greet())
+    option: int = int(input("Option? "))
+    points: int = 0
+    if option == 1:                         # 3 presenting three options and taking you to them
+        print(option_one())
+        print(extra_option())
+    else:
+        if option == 2:
+            print(option_two(points))
+            print(f"Your final score is -1 because you didn't even try {mad_emoji}")
+        else:
+            print(option_three())
+
+
+def greet() -> None:                            # 1 Procedure nameed greet
+    """Greeting to the player and introduction."""
+    player: str = str(input("What is your name? "))
+    print(f"Hello {player} and welcome to my guessing game! {intr}, {other}, {the_three}.")
+    return None
 
 
 def option_one() -> str:              # 4 procedure call
     """This is where the game will take place."""
-    print(f"Great {player} this is the main game! {op_one}")
+    print(f"Great this is the main game! {op_one}")
     points: int = 0
     while input("Letter? ") != "g":
         print(f"Not Quite {my_emoji} Keep guessing!")
@@ -46,7 +65,7 @@ def extra_option() -> str:
 
 def option_two(a: int) -> int:                  # 5 function with int parameter and return int
     """This is the bonus round."""
-    points: int = int(input(f"{player} how many tries did the first round take? "))
+    points: int = int(input("How many tries did the first round take? "))
     if a < 10:
         return points - 1
     else:
@@ -66,22 +85,6 @@ def option_three() -> str:
             return f"{points} guesses, you should have tried my game {mad_emoji}"
         else:
             return f"You suck, you took {points} guesses and didn't even try {mad_emoji}"
-
-
-def main() -> None:                         # 0 Main function that returns none
-    """Where everything is going down."""
-    print(greet())
-    option: int = int(input("Option? "))
-    points: int = 0
-    if option == 1:                         # 3 presenting three options and taking you to them
-        print(option_one())
-        print(extra_option())
-    else:
-        if option == 2:
-            print(option_two(points))
-            print(f"Your final score is -1 because you didn't even try {mad_emoji}")
-        else:
-            print(option_three())
 
 
 if __name__ == "__main__":      
