c9264fdfe675d091f72980481a8121646524182f
Aldair <asanch@ad.unc.edu>
2021-03-08 17:45:53 -0500

    progress

--- exercises/ex03/avoid_fifth.py
+++ exercises/ex03/avoid_fifth.py
@@ -1,12 +1,28 @@
 """EX03 - avoid_fifth function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730447704"
+
+
+def avoid_fifth(word: str) -> str:
+    """Function that takes out e and E."""
+    i = 0
+    output: str = ""
+    while i < len(word):
+        if word[i] == "E":
+            output += ""
+        else: 
+            if word[i] == "e":
+                output += ""
+            else:
+                output += str(word[i])
+        i += 1
+    return output
 
 
 def main() -> None:
     """Entrypoint of the program."""
-    # Put print statements here to test your function
-    # ex. print(avoid_fifth("hello there"))
+    print(avoid_fifth("hello there"))
+    print(avoid_fifth("Eventually Even"))
 
 
 if __name__ == "__main__":
--- exercises/ex03/palindromify.py
+++ exercises/ex03/palindromify.py
@@ -1,12 +1,30 @@
 """EX03 - palindromify function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730447704"
+
+
+def palindromify(statement: str, one_two: bool) -> str:
+    """Creates the palindromify"""
+    word: str = statement
+    if one_two is True:
+        i = len(statement)
+        while i > 0:
+            i = i - 1
+            word += statement[i]
+    else:
+        i = len(statement) - 1
+        while i > 0:
+            i = i - 1
+            word += statement[i]
+    return word
 
 
 def main() -> None:
     """Entrypoint of the program."""
-    # Put print statements here to test your function
-    # ex. print(palindromify("race", false))
+    print(palindromify("race", False))
+    print(palindromify("han", True))
+    print(palindromify("red", True))
+    print(palindromify("live on time", False))
 
 
 if __name__ == "__main__":
