09654eb725b0409ccff0c3542aed20e0863489af
Aldair <asanch@ad.unc.edu>
2021-03-18 18:24:13 -0400

    jupyter nb and importing

---
+++ lessons/helpers.py
@@ -0,0 +1,13 @@
+"""Example Functions that can be imported elsewhere"""
+
+
+THE_ANSWER: int = 42
+
+def shout(message: str) -> None:
+    print(message.upper())
+
+def whisper(message: str) -> None:
+    print(message.lower())
+
+print("helpers.py was evaluated")
+
